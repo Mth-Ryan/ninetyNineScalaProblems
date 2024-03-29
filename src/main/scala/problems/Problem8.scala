0038cb8e9f6f51d9eb81b5b718a4419133160507
Mateus Ryan <mthryan@protonmail.com>
2024-03-29 15:11:33 -0300

    Add Problem 8

---
+++ src/main/scala/problems/Problem8.scala
@@ -0,0 +1,35 @@
+package ninetyNineScalaProblems.problems
+
+import ninetyNineScalaProblems.core.*
+
+object Problem8 extends Problem {
+  val number = 8
+  val description = "Eliminate consecutive duplicates of list elements"
+
+  def rev[A](list: List[A]): List[A] = {
+    def aux(acc: List[A], list: List[A]): List[A] = list match {
+      case Nil  => acc
+      case h::t => aux(h::acc, t)
+    }
+    aux(List(), list)
+  }
+  
+  def dropWhile[A](list: List[A], condition: A => Boolean): List[A] = list match {
+    case list @ (h :: t) => if condition(h) then dropWhile(t, condition) else list
+    case Nil             => Nil
+  }
+  
+  def compress[A](list: List[A]): List[A] = {
+    def aux(acc: List[A], list: List[A]): List[A] = list match {
+      case h :: t => aux(h :: acc, dropWhile(t, _ == h))
+      case Nil    => acc
+    }
+    rev(aux(List(), list))
+  }
+
+  def exec(): Unit = {
+    val arg = List("a", "a", "b", "b", "c", "c")
+    logApply("compress", arg)
+    logResult(compress(arg))
+  }
+}
