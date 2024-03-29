bcfa839be07411a6701d891e341b4344c7fdcf07
Mateus Ryan <mthryan@protonmail.com>
2024-03-29 17:48:11 -0300

    Add Problem 10

---
+++ src/main/scala/problems/Problem10.scala
@@ -0,0 +1,35 @@
+package ninetyNineScalaProblems.problems
+
+import ninetyNineScalaProblems.core.*
+import scala.annotation.tailrec
+
+object Problem10 extends Problem {
+
+  override protected val number: Int = 10
+  override protected val description: String = "Run-length encoding of a list."
+
+  def rev[A](list: List[A]): List[A] = {
+    @tailrec
+    def aux(acc: List[A], list: List[A]): List[A] = list match {
+      case Nil  => acc
+      case h::t => aux(h::acc, t)
+    }
+    aux(List(), list)
+  }
+
+  def encode[A](list: List[A]): List[(Int, A)] = {
+    @tailrec
+    def aux(count: Int, acc: List[(Int, A)], list: List[A]): List[(Int, A)] = list match {
+      case Nil => Nil
+      case List(a) => (count + 1, a) :: acc
+      case a :: (t @ (b :: _)) => if a == b then aux(count + 1, acc, t) else aux(0, (count + 1, a) :: acc, t)
+    }
+    rev(aux(0, Nil, list))
+  }
+
+  override protected def exec(): Unit = {
+    val args = List("a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e")
+    logApply("encode", args)
+    logResult(encode(args))
+  }
+}
