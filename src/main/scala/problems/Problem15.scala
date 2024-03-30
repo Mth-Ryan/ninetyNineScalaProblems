9a5bca4444f2c8506d93ab9a4e973857e0e4fb63
Mateus Ryan <mthryan@protonmail.com>
2024-03-30 13:36:27 -0300

    Add Problem 15

--- src/main/scala/problems/Problem15.scala
+++ src/main/scala/problems/Problem15.scala
@@ -1,1 +1,25 @@
+package ninetyNineScalaProblems.problems
 
+import ninetyNineScalaProblems.core.*
+import scala.annotation.tailrec
+
+object Problem15 extends Problem {
+
+  override protected val number: Int = 15
+  override protected val description: String = "Replicate the elements of a list a given number of times"
+
+  def replicate[A](list: List[A], n: Int): List[A] = {
+    @tailrec
+    def aux(acc: List[A], list: List[A], n: Int): List[A] = list match {
+      case Nil    => acc
+      case h :: t => aux(acc ::: Problem12.repeat(h, n), t, n)
+    }
+    aux(Nil, list, n)
+  }
+
+  override protected def exec(): Unit = {
+    val args = List("a","b", "c", "d", "e")
+    logApply("replicate", args, 3)
+    logResult(replicate(args, 3))
+  }
+}
